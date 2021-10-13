968e6e86138efcecf515620ddb403dbe7d0ab167
Meryem <mhamdi@adobe.com>
2021-10-12 18:21:25 -0700

    added another adapter architecture

--- contlearnalg/adapter.py
+++ contlearnalg/adapter.py
@@ -44,7 +44,7 @@ class Activation_Function_Class(nn.Module):
         return self.f(x)
 
 
-class Adapter(nn.Module):
+class MADX(nn.Module):
     """
     Implementation of a single Adapter block.
     """
@@ -91,20 +91,20 @@ class Adapter(nn.Module):
 
         # sequential adapter, first downproject, then non-linearity then upsample. In the forward pass we include the
         # residual connection
-        self.adapter_down = nn.Sequential(*seq_list)
+        self.adapter_down_1 = nn.Sequential(*seq_list)
 
         # Up projection to input size
-        self.adapter_up = nn.Linear(self.down_sample, self.embed_dim)
+        self.adapter_up_1 = nn.Linear(self.down_sample, self.embed_dim)
 
         # If we want to have a layer norm on output, we apply it later after a separate residual connection
         # This means that we learn a new output layer norm, which replaces another layer norm learned in the bert layer
         if self.add_layer_norm_after:
-            self.adapter_norm_after = nn.LayerNorm(self.input_size)
+            self.adapter_norm_after_1 = nn.LayerNorm(self.input_size)
 
         # if we want to initialize with the bert strategy then this function is called for all the linear layers
         if init_bert_weights:
-            self.adapter_down.apply(self.init_bert_weights)
-            self.adapter_up.apply(self.init_bert_weights)
+            self.adapter_down_1.apply(self.init_bert_weights)
+            self.adapter_up_1.apply(self.init_bert_weights)
 
     def forward(self, x, residual_input=1.0):  # , residual_input=None):
         # print("x.shape:", x.shape)
@@ -113,9 +113,9 @@ class Adapter(nn.Module):
         # print("self.adapter_norm_before:", self.adapter_norm_before)
         #
         # print("self.adapter_norm_before(x):", self.adapter_norm_before(x))
-        down = self.adapter_down(x)
+        down = self.adapter_down_1(x)
 
-        up = self.adapter_up(down)
+        up = self.adapter_up_1(down)
 
         output = up
 
@@ -125,8 +125,130 @@ class Adapter(nn.Module):
 
         # apply layer norm if available
         if self.add_layer_norm_after:
-            output = self.adapter_norm_after(output)
+            output = self.adapter_norm_after_1(output)
 
+        # if residual should be applied after layer norm, apply it here
+        if not self.residual_before_ln:
+            output = output + residual_input
+
+        return output, down, up
+
+    # This is copied from the BertPreTrainedModel class to make this a self containing class.
+    @staticmethod
+    def init_bert_weights(module):
+        """Initialize the weights."""
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # std defaults to 0.02, this might need to be changed
+            module.weight.data.normal_(mean=0.0, std=0.02)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
+
+class Houlsby(nn.Module):
+    """
+    Implementation of a single Adapter block.
+    """
+
+    def __init__(
+            self,
+            input_size,
+            down_sample=None,
+            non_linearity="relu",
+            init_bert_weights=True,
+            add_layer_norm_before=True,
+            add_layer_norm_after=True,
+            residual_before_ln=True, # could try with
+    ):
+        super().__init__()
+
+        self.input_size = input_size
+        self.add_layer_norm_before = add_layer_norm_before
+        self.add_layer_norm_after = add_layer_norm_after
+        self.residual_before_ln = residual_before_ln
+
+        self.embed_dim = input_size
+
+        # list for all modules of the adapter, passed into nn.Sequential()
+        seq_list_1 = []
+
+        # If we want to have a layer norm on input, we add it to seq_list
+        if self.add_layer_norm_before:
+            self.adapter_norm_before = nn.LayerNorm(self.input_size)
+            seq_list_1.append(self.adapter_norm_before)
+
+        # if a downsample size is not passed, we just half the size of the original input
+        self.down_sample = down_sample
+        if down_sample is None:
+            self.down_sample = self.embed_dim // 2
+
+        # Linear down projection of the input
+        seq_list_1.append(nn.Linear(self.embed_dim, self.down_sample))
+
+        # select non-linearity
+        self.non_linearity = Activation_Function_Class(non_linearity.lower())
+
+        seq_list_1.append(self.non_linearity)
+
+        # sequential adapter, first downproject, then non-linearity then upsample. In the forward pass we include the
+        # residual connection
+        self.adapter_down_1 = nn.Sequential(*seq_list_1)
+
+        # Up projection to input size
+        self.adapter_up_1 = nn.Linear(self.down_sample, self.embed_dim)
+
+        # If we want to have a layer norm on output, we apply it later after a separate residual connection
+        # This means that we learn a new output layer norm, which replaces another layer norm learned in the bert layer
+        self.adapter_norm_after_1 = nn.LayerNorm(self.input_size)
+
+        # if we want to initialize with the bert strategy then this function is called for all the linear layers
+        if init_bert_weights:
+            self.adapter_down_1.apply(self.init_bert_weights)
+            self.adapter_up_1.apply(self.init_bert_weights)
+
+        self.feed_layer_1 = nn.Linear(self.embed_dim, self.embed_dim)
+        self.feed_layer_2 = nn.Linear(self.embed_dim, self.embed_dim)
+
+        seq_list_2=[]
+        seq_list_2.append(nn.Linear(self.embed_dim, self.down_sample))
+        seq_list_2.append(Activation_Function_Class(non_linearity.lower()))
+        self.adapter_down_2 = nn.Sequential(*seq_list_2)
+
+        self.adapter_up_2 = nn.Linear(self.down_sample, self.embed_dim)
+
+        self.adapter_norm_after_2 = nn.LayerNorm(self.input_size)
+
+    def forward(self, x, residual_input=1.0):  # , residual_input=None):
+        # print("x.shape:", x.shape)
+        # print("self.adapter_down:", self.adapter_down)
+        #
+        # print("self.adapter_norm_before:", self.adapter_norm_before)
+        #
+        # print("self.adapter_norm_before(x):", self.adapter_norm_before(x))
+        down = self.adapter_down_1(x)
+
+        up = self.adapter_up_1(down)
+
+        output = up
+
+        # apply residual connection before layer norm if configured in this way
+        if self.residual_before_ln:
+            output = output + residual_input
+
+        output = self.adapter_norm_after_1(output)
+
+        output = self.feed_layer_1(output)
+        output = self.feed_layer_2(output)
+
+        down = self.adapter_down_2(output)
+
+        up = self.adapter_up_2(down)
+
+        output = up
+
+        output = self.adapter_norm_after_2(output)
         # if residual should be applied after layer norm, apply it here
         if not self.residual_before_ln:
             output = output + residual_input
