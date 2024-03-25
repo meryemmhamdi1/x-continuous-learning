c03a43525267b7981f4fcc651253755d5adc7852
Meryem M'hamdi <meryemmhamdi1@gmail.com>
2024-03-24 19:27:16 -0700

    Cleaning some files after restructuring

--- tests/scripts/tydiqa.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python tests/test_base_models_main.py --order_lst "en_ru_ar_id_fi_bn_te_sw" --lt_queue_mode "cont-mono" --task_name "qa" --data_name "tydiqa" --data_format "json"
